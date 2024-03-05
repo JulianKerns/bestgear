7ede3fbd7cc322ffeb3adc886860b9b63eba099b
JulianKerns <j.kernstock@gmx.at>
2024-03-05 14:24:22 +0100

    added user CLI inputs to change the API URL and the URL we are scraping the data from

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 const{getInputData} = require('./fetch_data.js')
+const {scraping} = require('./scraping.js')
 
 const item_slots = ["Head","Neck","Shoulder","Back","Chest","Waist","Wrist","Hands","Legs","Feet","Finger1","Finger2","Mainhand","Offhand"]
 async function main(){
@@ -7,7 +8,7 @@ async function main(){
         let inputDataRe = await getInputData()
          
 
-        const response = await fetch(`https://raider.io/api/v1/characters/profile?region=${inputDataRe[0]}&realm=${inputDataRe[1]}&name=${inputDataRe[2]}&fields=gear`)
+        const response = await fetch(`https://raider.io/api/v1/characters/profile?region=${inputDataRe[0].toLowerCase()}&realm=${inputDataRe[1].toLowerCase()}&name=${inputDataRe[2].toLowerCase()}&fields=gear`)
         const data = await response.json()
       
         for(item of item_slots){
@@ -20,9 +21,17 @@ async function main(){
                 continue
             }
             console.log(`Geartype: ${item}  Name: ${itemInfo.name}  ItemLevel: ${itemInfo.itemlvl}`)
-    
+
+            
         }
-    
+        console.log(data.class)
+        const scrapedData = await scraping(data.class, data.active_spec_name)
+
+        //const charInfo = {
+           // class : `${data.class}`.toLowerCase(),
+            //spec : `${data.active_spec_name}`.toLowerCase()
+         // }
+        console.log(scrapedData)
     }
 main()
 
--- scraping.js
+++ scraping.js
@@ -1,36 +1,53 @@
 // Getting Data from Wowhead directly
 
 const puppeteer = require('puppeteer')
-const url = 'https://www.wowhead.com/guide/classes/monk/windwalker/bis-gear'
 
-async function scraping(){
-    let i = 0
+
+async function scraping(charClass, charSpec){
+    if (charClass === 'Death Knight'){
+        charClass = 'death-knight' 
+    }
+    if (charClass === 'Demon Hunter'){
+        charClass = 'demon-hunter'
+    }
+
+    let url = `https://www.wowhead.com/guide/classes/${charClass}/${charSpec}/bis-gear`
+    console.log(url)
     let bestgear = []
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
    
-    await Promise.all([page.goto(url), page.waitForSelector('#guide-body', {timeout: 5_000}),page.waitForSelector('tr:nth-child(14)', {timeout: 5_000})])
+    try{await Promise.all([page.goto(url), page.waitForSelector('#guide-body', {timeout: 10_000}),page.waitForSelector('tr:nth-child(14)', {timeout: 10_000})])
         bestgear = await page.evaluate(() => {
         return Array.from(document.querySelectorAll('#guide-body > div:nth-child(34) > table > tbody > tr > td:nth-child(2) > a > span')).map(x => x.textContent)   
     })
-
-    while (bestgear.length === 0 && i < 20){
-        await Promise.all([page.goto(url), page.waitForSelector('#guide-body', {timeout: 1000_000}),page.waitForSelector('tr:nth-child(14)', {timeout: 1000_000})])
+        let i = 0
+        while (bestgear.length === 0 && i < 20){
+        await Promise.all([page.goto(url), page.waitForSelector('#guide-body', {timeout: 100_000}),page.waitForSelector('tr:nth-child(14)', {timeout: 100_000})])
         i++
         console.log('Could not scrape the data... Trying again!')
             bestgear = await page.evaluate(() => {
             return Array.from(document.querySelectorAll('#guide-body > div:nth-child(34) > table > tbody > tr > td:nth-child(2) > a > span')).map(x => x.textContent) 
-        
-    })}
-    if(bestgear.length===0){
-        console.log('Could not scrape the data!')
+        })
+        }
+        if(bestgear.length===0){
+            console.log(`Could not scrape the data!`)
+        }
+    }
+
+
+    catch(err){ 
+        console.log(`Could not scrape the data! Please enter character info again! ${err.messge}`)
     }
 
     await browser.close()
-    console.log(bestgear)
+    return bestgear
    
     
 }
 
-scraping()
 
+
+module.exports={
+    scraping
+}
